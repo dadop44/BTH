b88518b8a7e7fce346fd2a55a3ed3abd9706ec77
dadop44 <58806599+dadop44@users.noreply.github.com>
2020-12-26 15:56:30 +0700

    update

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,14 +1,14 @@
 import 'package:bth/services/firestore_services.dart';
-//import 'package:bth/services/authentication_service.dart';
+import 'package:bth/services/authentication.dart';
 import 'package:get_it/get_it.dart';
-//import 'package:bth/services/navigation_service.dart';
-//import 'package:bth/services/dialog_service.dart';
+import 'package:bth/services/navigation_services.dart';
+import 'package:bth/services/dialog_services.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
-  //locator.registerLazySingleton(() => NavigationService());
-  //locator.registerLazySingleton(() => DialogService());
-  //locator.registerLazySingleton(() => AuthenticationService());
+  locator.registerLazySingleton(() => NavigationService());
+  locator.registerLazySingleton(() => DialogService());
+  locator.registerLazySingleton(() => AuthenticationService());
   locator.registerLazySingleton(() => FireStoreService());
 }
